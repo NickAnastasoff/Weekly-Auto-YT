34d81646a840a2798c5bbd15d20c189b46d4dff2
Nick Anastasoff <nickanastasoff@Nicks-Mac.local>
2024-01-26 21:28:23 -0500

    working with images

--- Week2/main.py
+++ Week2/main.py
@@ -3,6 +3,8 @@ import random
 import moviepy.editor as mp
 from constants import *
 from moviepy.editor import concatenate_videoclips
+from pexels import get_best_images
+import os
 
 def get_random_choices():
     """
@@ -14,7 +16,7 @@ def get_random_choices():
     return choices
 
 
-def text_box(text="text", fontsize=40, back_opacity=.5, y=0, width=0, height=0,
+def text_box(text="text", fontsize=40, opac=.5, y=0, width=0, height=0,
              duration=5, start_time=0, w=1080, h=1920):
     """
     Creates a text clip with a background color and a black outline.
@@ -22,7 +24,7 @@ def text_box(text="text", fontsize=40, back_opacity=.5, y=0, width=0, height=0,
     Args:
         text: The text to be displayed.
         fontsize: The size of the text.
-        back_opacity: The opacity of the background.
+        opac: The opacity of the background.
         y: The y position of the text.
         width: The width of the background.
         height: The height of the background.
@@ -43,7 +45,7 @@ def text_box(text="text", fontsize=40, back_opacity=.5, y=0, width=0, height=0,
     )
     text_clip = text_clip.on_color(
         size=(text_clip.w, text_clip.h + 20), color=(0, 0, 0), 
-        pos=(0, 'center'), col_opacity=back_opacity
+        pos=(0, 'center'), col_opacity=opac
     )
     text_clip = text_clip.set_pos(
         lambda t: ((w - text_clip.w) / 2, (h - text_clip.h) / y)
@@ -64,11 +66,33 @@ def text_clip_from_choices(choices):
     text_clips = []
     for i, choice in enumerate(choices):
         text_clip = text_box(
-            choice[0], fontsize=80, back_opacity=0.0, y=(i * 40) + 1.5,
+            choice[0], fontsize=80, opac=0.0, y=(i * 40) + 1.5,
             width=0, height=500, duration=video_length, start_time=0
         )
         text_clips.append(text_clip)
     return text_clips
+
+def resized_image_clip(image, duration=5, start_time=0, h=384, y=0):
+    """
+    Creates a resized image clip with a black outline.
+
+    Args:
+        image: The image to be displayed.
+        duration: The duration of the clip.
+        start_time: The start time of the clip.
+        h: The height of the video.
+        y: The y position of the image.
+
+    Returns:
+        A resized image clip with the specified properties.
+    """
+    image_clip = mp.ImageClip(image).set_duration(duration)
+    image_clip = image_clip.set_start(start_time)
+    image_clip = image_clip.set_pos(
+        lambda _: ((video_width - image_clip.w) / 2, y)
+    )
+    return image_clip
+
 
 def make_background(top):
     """
@@ -87,14 +111,22 @@ def main():
     final_clips = []
     for iteration in range(iterations):
         choices = get_random_choices()
+        # find longest words in sentence
+        longest_words = [sorted(choice[0].split(), key=len, reverse=True) for choice in choices]
+        get_best_images(longest_words)
+        image_clips = resized_image_clip("Week2/images/0.jpg", y=384*1.5), resized_image_clip("Week2/images/1.jpg", y=384*3.5)
         text_clips = text_clip_from_choices(choices)
         background = make_background(random.randint(0, 1))
 
-        final_clip = mp.CompositeVideoClip([background, *text_clips])
+        final_clip = mp.CompositeVideoClip([background, *image_clips, *text_clips])
         final_clips.append(final_clip)
 
     combined_clip = concatenate_videoclips(final_clips)
     combined_clip.write_videofile(f'{path}/would-you-rather.mp4', fps=24, codec='libx264')
+    
+    # delete images folder
+    os.system("rm -rf Week2/images")
+
 
 
 if __name__ == "__main__":
---
+++ Week2/pexels.py
@@ -0,0 +1,98 @@
+import requests
+import os
+import PIL
+import io
+PEXELS_API_KEY = os.environ.get("PEXELS_API_KEY")
+
+
+def search_videos(query_string, orientation_landscape=True):
+    url = "https://api.pexels.com/videos/search"
+    headers = {
+        "Authorization": PEXELS_API_KEY
+    }
+    params = {
+        "query": query_string,
+        "orientation": "landscape" if orientation_landscape else "portrait",
+        "per_page": 15
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    json_data = response.json()
+    return json_data
+
+
+def get_best_video(query_string, orientation_landscape=True, used_vids=[]):
+    vids = search_videos(query_string, orientation_landscape)
+    videos = vids['videos']  # Extract the videos list from JSON
+
+    # Filter and extract videos with width and height as 1920x1080 for landscape or 1080x1920 for portrait
+    if orientation_landscape:
+        filtered_videos = [video for video in videos if video['width'] >= 1920 and video['height'] >= 1080 and video['width']/video['height'] == 16/9]
+    else:
+        filtered_videos = [video for video in videos if video['width'] >= 1080 and video['height'] >= 1920 and video['height']/video['width'] == 16/9]
+
+    # Sort the filtered videos by duration in ascending order
+    sorted_videos = sorted(filtered_videos, key=lambda x: abs(15-int(x['duration'])))
+
+    # Extract the top 3 videos' URLs
+    for video in sorted_videos:
+        for video_file in video['video_files']:
+            if orientation_landscape:
+                if video_file['width'] == 1920 and video_file['height'] == 1080:
+                    if not (video_file['link'].split('.hd')[0] in used_vids):
+                        return video_file['link']
+            else:
+                if video_file['width'] == 1080 and video_file['height'] == 1920:
+                    if not (video_file['link'].split('.hd')[0] in used_vids):
+                        return video_file['link']
+    print("NO LINKS found for this round of search with query :", query_string)
+    return None
+
+def search_images(query_string):
+    url = "https://api.pexels.com/v1/search"
+    headers = {
+        "Authorization": PEXELS_API_KEY
+    }
+    params = {
+        "query": query_string,
+        "per_page": 1
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    json_data = response.json()
+    return json_data
+
+def get_best_images(query_strings):
+    if not os.path.exists("Week2/images"):
+        os.mkdir("Week2/images")
+    for i, query in enumerate(query_strings):
+        for _ in query:
+            try:
+                imgs = search_images(query)
+                image = imgs['photos'][0]['src']['original']
+                image = requests.get(image).content
+                break
+            except:
+                print("NO LINKS found for this round of search with query :", query)
+                continue
+        # resize to 384 height
+        image = PIL.Image.open(io.BytesIO(image))
+        w, h = image.size
+        new_h = 384
+        new_w = int(new_h * w / h)
+        image = image.resize((new_w, new_h))
+
+        # put in Week2/images
+        image.save(f"Week2/images/{i}.jpg")
+
+#     image_urls = []
+# json_data = search_images("c")
+# for photo in json_data['photos']:
+#     image_urls.append(photo['src']['large'])
+
+# if not os.path.exists("Week2/images"):
+#     os.mkdir("Week2/images")
+# # download to Week2/images
+# response = requests.get(image_urls[0])
+# with open(f"Week2/images/0.jpg", "wb") as f:
+#     f.write(response.content)
